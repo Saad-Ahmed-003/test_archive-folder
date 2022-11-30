44ef8c134e5557e7bbe17994f5a5641d7eee7414
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-11-30 20:42:59 +0530

    Create untitled-14.py

---
+++ untitled-14.py
@@ -0,0 +1,22 @@
+n = int(input("number :- "))
+
+mach = n % 1
+mach2 = n % n
+
+print(type(mach), type(mach2))
+
+if mach == 0 and mach2 == 0:
+    print("It's a prime number.")
+else:
+    print("It's not a prime number")
+
+devided_numbers = []
+
+for i in range(2, n):
+    devided_numbers += str(n % i)
+    print(devided_numbers)
+
+if 0 in devided_numbers:
+    print("It's not a prime number")
+else:
+    print("It's a prime number.")
