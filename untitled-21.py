890e2d75551714fa35af8d294b90e47c07808930
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-04 23:27:30 +0530

    Update untitled-21.py

--- untitled-21.py
+++ untitled-21.py
@@ -1,1 +1,18 @@
-D_List = [ {"saad" : 1}]
+D_List = [ {"a" : 1}, {"b" : 2}, {"c" : 3}, {"d" : 4}, {"e" : 5}]
+
+all_auctions = []
+
+for i in D_List:
+    for x in i:
+        all_auctions.append(i[x])
+
+highest_number = 0
+
+print(all_auctions)
+
+for i in all_auctions:
+    if highest_number < i :
+        highest_number = i
+
+print(highest_number)
+
