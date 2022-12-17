553167fa9727a43a08db0c1e019caedf173ef337
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-17 16:54:33 +0530

    Create untitled.py

---
+++ blackjack/untitled.py
@@ -0,0 +1,12 @@
+List1 = [1, 2]
+List2 = [3, 4]
+
+def check_the_Answer(d1, d2):
+    if sum(d1) < 21:
+        print(d1)
+    if sum(d2) < 21:
+        print(d2)
+
+
+
+check_the_Answer(List1, List2)
