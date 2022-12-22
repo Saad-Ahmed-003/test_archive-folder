360c8c1108d1a73e50e110c1aecbaf9bc823c245
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-22 17:59:27 +0530

    Update untitled.py

--- number game/untitled.py
+++ number game/untitled.py
@@ -1,3 +1,16 @@
+import random
+guess_the_number = random.choice(range(1,101))
+data = 9
+print(guess_the_number)
 
-for i in range(1, 5):
-    print(i)
+while data > 0:
+    print(data)
+    data1 = int(input("input"))
+    if data1 < guess_the_number:
+        print("to")
+    elif data1 > guess_the_number:
+        print("low")
+    elif data1 == guess_the_number:
+        print("yes")
+    data -= 1
+
