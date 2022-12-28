d4fb479d7f54e3def934fbc146bdc84a2924660f
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-28 21:58:26 +0530

    coffee

--- untitled 24/oop-coffee-machine-start/main.py
+++ untitled 24/oop-coffee-machine-start/main.py
@@ -1,3 +1,7 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+menu = Menu()
+latte = MenuItem(name="latte", water=100, milk=250, coffee=24, cost=2.5)
+a = menu.get_items()
+print(menu.get_items())
