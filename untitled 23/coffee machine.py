1f1372993c07270958006c5cf6342e01af901efe
saad ahmed bin muhammed sajid pasha <117589869+Saad-Ahmed-003@users.noreply.github.com>
2022-12-28 16:50:55 +0530

    coffe

--- untitled 23/coffee machine.py
+++ untitled 23/coffee machine.py
@@ -1,26 +1,33 @@
-resources = dict(milk=1000, water=1500, coffee=300)
+resources = dict(milk=250, water=250, coffee=24)
 amount = dict(espresso=1.5, latte=2.5, cappuccino=3.0)
 coin = dict(Penney=0.01, Nickel=0.05, Dime=0.10, Quarter=0.25)
+latte_1 = dict(water=200, milk=150, coffee=24)
+espresso_1 = dict(water=50, coffee=18)
+cappuccino_1 = dict(water=250, milk=100, coffee=24)
 coins = []
 your_coins = 0
+brake_point = 0
 is_on = True
 
 
 def espresso():
     resources["water"] -= 50
     resources["coffee"] -= 18
+    print(f"storage: {resources}")
 
 
 def latte():
     resources["water"] -= 200
     resources["milk"] -= 150
     resources["coffee"] -= 24
+    print(f"storage: {resources}")
 
 
 def cappuccino():
     resources["water"] -= 250
     resources["milk"] -= 100
     resources["coffee"] -= 24
+    print(f"storage: {resources}")
 
 
 def coffee(name):
@@ -32,16 +39,24 @@ def coffee(name):
         cappuccino()
 
 
+def check_resources(f_name):
+    if f_name == "latte":
+        f_name = latte_1
+        for i in f_name:
+            if f_name[i] < resources[i]:
+                print(f"sorry there is not enough {i}.")
+
+
 def check_amount(money, coffee_type):
     total_coins = 0
     for i in range(money[0]):
         total_coins += coin["Quarter"]
     for i in range(money[1]):
-        total_coins += coin["Dimes"]
+        total_coins += coin["Dime"]
     for i in range(money[2]):
-        total_coins += coin["Nickels"]
+        total_coins += coin["Nickel"]
     for i in range(money[3]):
-        total_coins += coin["Pennies"]
+        total_coins += coin["Penney"]
     if total_coins < amount[coffee_type]:
         print("Sorry that's not enough. Money refunded.")
     elif total_coins > amount[coffee_type]:
@@ -49,8 +64,26 @@ def check_amount(money, coffee_type):
         print(f"here is ${total_coins} in change.")
 
 
+print(f"storage: {resources}")
+
 while is_on:
     question = input("  What would you like? (espresso/latte/cappuccino): ")
+    if question == "latte":
+        print(f'  water : {latte_1["water"]}')
+        print(f'  milk : {latte_1["milk"]}')
+        print(f'  coffee : {latte_1["coffee"]}')
+        print(f'  price : ${amount[question]}')
+    elif question == "espresso":
+        print(f'  water : {espresso_1["water"]}')
+        print(f'  coffee : {espresso_1["coffee"]}')
+        print(f'  price : ${amount[question]}')
+    elif question == "cappuccino":
+        print(f'  water : {cappuccino_1["water"]}')
+        print(f'  milk : {cappuccino_1["milk"]}')
+        print(f'  coffee : {cappuccino_1["coffee"]}')
+        print(f'  price : ${amount[question]}')
+    else:
+        break
     print("Please insert coins.")
     coins.append(int(input("how manny quarters?: ")))
     coins.append(int(input("how manny dimes?: ")))
--- untitled 23/test.py
+++ untitled 23/test.py
@@ -1,5 +1,8 @@
 a = 2.5
 b = 3.5
+c = {"a": 1, "b": 2, "c": 3}
+d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+e = {"a": 1, "b": 2, "c": 3}
 
 if a < b:
     print("WTF")
@@ -14,3 +17,6 @@ if a < b:
     print("it worked")
 elif b < a:
     print("this works")
+for i in c:
+    if e[i] == c[i]:
+        print(i)
