f6713e581888cd751ed49a15ad06d1e91e4ddb7c
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-05 19:07:06 +0530

    daly

--- untitled 26/main.py
+++ untitled 26/main.py
@@ -6,13 +6,6 @@ screen.setup(width=800, height=600)
 screen.title("pong")
 screen.tracer(0)
 
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
-
-game_is_on = True
-while game_is_on:
-    screen.update()
-
-screen.exitonclick()
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
