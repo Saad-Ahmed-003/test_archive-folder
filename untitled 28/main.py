3fec4f074ec3678fd01105c19c9043af6745f17a
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-13 01:04:03 +0530

    Update main.py

--- untitled 28/main.py
+++ untitled 28/main.py
@@ -1,7 +1,13 @@
-import csv
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature_number = []
-    for row in data:
-        print(row)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+average = sum(temp_list) / len(temp_list)
+print(average)
