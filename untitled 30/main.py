503fffab401f1d04a3d91e278c6929eca2867bc6
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-01-31 01:56:51 +0530

    Update main.py

--- untitled 30/main.py
+++ untitled 30/main.py
@@ -1,10 +1,22 @@
 list_1 = {
-    1: [1, 2],
-    2: [1, 2]
+    "name": {0: "test", 1: "saad"},
+    "email": {0: "test@mail.com", 1: "saadbinsajid03@gmail.com"},
+    "year": {0: 1996, 1: 2023},
+    "month": {0: 12, 1: 1},
+    "day": {0: 1, 1: 31}
 }
 
+tuple_1 = ()
+tuple_2 = ()
+
+
 for i in list_1:
-    print(list_1[i][0])
+    for x in list_1[i]:
+        if x == 0:
+            tuple_1 += list_1[i][x]
+        elif x == 1:
+            tuple_2 += list_1[i][x]
 
-
+print(tuple_1)
+print(tuple_2)
 
