e11318160d4e0bfa530d43a3cc46f85573e98804
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-02-01 20:50:29 +0530

    Update main.py

--- untitled 31/main.py
+++ untitled 31/main.py
@@ -1,6 +1,18 @@
 import requests
 
-response = requests.get(url="https://api.kanye.rest/")
+MY_LAT = 19.180267
+MY_LONG = 77.308044
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+    }
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 data = response.json()
 
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 print(data)
