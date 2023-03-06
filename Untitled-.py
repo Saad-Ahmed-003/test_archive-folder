37891f1ded865f7606b385bf56973a2c2134a33b
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-03-06 22:50:06 +0530

    Update Untitled-.py

--- Untitled-.py
+++ Untitled-.py
@@ -1,11 +1,10 @@
-#//how to print 4 items from a list using for loop in python?
-mylist = [[1,2,3],[4,5,6,7],[8,9,10]]
-for x in mylist:
-    if len(x)==3:
-        print(x)
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
-x = "5 degrees"
-cel = 0
-y = float(x)
-z = (x-32.0)*5.0/9.0
-print(z)
+dictionary = {}
+
+for i in range(len(list1)):
+    print(i)
+    dictionary[i] = {"name": list2[i], "time": list1[i]}
+
+print(dictionary)
