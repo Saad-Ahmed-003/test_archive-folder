cfdd818e798f032092dca7c6b9ba03925309b48d
Saad-Ahmed-003 <117589869+Saad-Ahmed-003@users.noreply.github.com>
2023-05-05 16:13:24 +0530

    a

--- sql testing python/main.py
+++ sql testing python/main.py
@@ -1,67 +1,11 @@
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# create database
-conn = sqlite3.connect('teacher.db')
-
-name = "afifa"
-a = "a"
-b = "b"
-email = "saad@212"
-num = "1234567890"
-
-# create cursor
-c = conn.cursor()
-
-# create a table
-c.execute("""CREATE TABLE IF NOT EXISTS customers(
-    name text,
-    username text,
-    email text
-    )""")
-
-c.execute(f"SELECT rowid, * FROM customers WHERE name = '{name}'")
-items1 = c.fetchall()
-print(items1[0][2])
-
-
-conn.commit()
-# commit code
-conn.close()
-
-
-def reade_teachers():
-    conn1 = sqlite3.connect('teacher.db')
-    cur = conn1.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS customers(
-        name text,
-        user text,
-        password text,
-        address text,
-        email text,
-        phone_number text 
-    )
-    """)
-    cur.execute("SELECT rowid, * FROM customers")
-    data = cur.fetchall()
-    list1 = [i[2] for i in data]
-    list2 = data
-    conn1.commit()
-    conn1.close()
-    all_list = (list1, list2)
-    return all_list
-
-
-passe = []
-man = reade_teachers()
-for i in man[1]:
-    passe.append(i[3])
-
-dictionary = {k: v for (k, v) in zip(man[0], passe)}
-
-print(dictionary)
-
-ad = dictionary["saad♥❤ahmed03"]
-print(ad)
-
-if "ahmed" in dictionary:
-    print(dictionary["ahmed"])
+# create the extension
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# config the sqlite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
